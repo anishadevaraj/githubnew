e7f0bea568189295001d447ce3b7d477b52e7a5a
anishadevaraj <160848574+anishadevaraj@users.noreply.github.com>
2024-02-23 16:22:33 +0530

    initial class

---
+++ src/githubnewpackage/newclass.java
@@ -0,0 +1,13 @@
+package githubnewpackage;
+
+public class newclass {
+
+	public static void main(String[] args) {
+		int a=60, b=40;
+		int f = (a==b)? a:b;     //var = exp? result(T)1:result(F)2;=
+		System.out.println(f);
+		
+
+	}
+
+}
