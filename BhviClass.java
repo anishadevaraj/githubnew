ab2a622cdd08fd18ada9c09544765bc983f8c46d
anishadevaraj <160848574+anishadevaraj@users.noreply.github.com>
2024-02-23 18:07:42 +0530

    Add files via upload
    Signed-off-by: anishadevaraj <160848574+anishadevaraj@users.noreply.github.com>

---
+++ BhviClass.java
@@ -0,0 +1,12 @@
+package bhuviPack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BhviClass {
+
+	public static void main(String[] args) {
+		char ch;
+		for (ch='1';ch <='9';ch++)
+		System.out.println(ch);
+	}}
